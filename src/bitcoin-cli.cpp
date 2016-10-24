0511f37582183b297ee621a81fa9fa6709316b0a
merelcoin <zottejos+merelcoin@gmail.com>
2016-10-24 13:52:06 +0200

    Update bitcoin-cli.cpp

--- src/bitcoin-cli.cpp
+++ src/bitcoin-cli.cpp
@@ -67,7 +67,7 @@ static bool AppInitRPC(int argc, char* argv[])
     //
     ParseParameters(argc, argv);
     if (argc<2 || mapArgs.count("-?") || mapArgs.count("-help") || mapArgs.count("-version")) {
-        std::string strUsage = _("Litcoin Core RPC client version") + " " + FormatFullVersion() + "\n";
+        std::string strUsage = _("Merelcoin Core RPC client version") + " " + FormatFullVersion() + "\n";
         if (!mapArgs.count("-version")) {
             strUsage += "\n" + _("Usage:") + "\n" +
                   "  merelcoin-cli [options] <command> [params]  " + _("Send command to Merelcoin Core") + "\n" +
